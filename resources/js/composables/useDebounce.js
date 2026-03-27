460695e8dad521b81ec4f7630394a3baf8f7740d
ventum-EA <138501370+ventum-EA@users.noreply.github.com>
2026-03-27 10:00:00 +0200

    patch: added missing components for composables

---
+++ resources/js/composables/useDebounce.js
@@ -0,0 +1,9 @@
+import { ref } from "vue";
+
+export function useDebounce(fn, delay = 300) {
+    let timer = null;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), delay);
+    };
+}
