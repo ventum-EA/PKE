7e992546643db7e2b8fd15d78518a3c215121e1a
ventum-EA <138501370+ventum-EA@users.noreply.github.com>
2026-03-28 10:00:00 +0200

    feat: wrote main services

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Šaha Analīzes Platforma</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-[#0c0c0e]">
+    <div id="app"></div>
+</body>
+</html>
