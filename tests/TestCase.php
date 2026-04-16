b7c81d4e33b99219cdc42c5935cb8b346ca5b7b9
ventum-EA <138501370+ventum-EA@users.noreply.github.com>
2026-04-16 10:30:00 +0300

    patch: fix CI; Laravel 12 TestCase and workflow corrections

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,10 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function createApplication()
-    {
-        $app = require __DIR__ . '/../bootstrap/app.php';
-        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-        return $app;
-    }
+    //
 }
